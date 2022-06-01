9b10c70c87bff382b6a922c2d5e6221cb50cebdd
alvaradocx <42775228+alvaradocx@users.noreply.github.com>
2022-06-01 14:59:17 -0400

    Update snp_search.py

--- os_apps/snp_search.py
+++ os_apps/snp_search.py
@@ -104,7 +104,7 @@ def app():
     main_snp_list = list(main_df['topSNP'].unique())
     st.session_state['snp_list']  = main_snp_list
 
-    st.title('GeneSNP
+    st.title('SNP Searchh')
     st.write("Use this interactive tool to search for your SNP(s) of interest against our database.")
     
     with st.form("SNP_Search"):

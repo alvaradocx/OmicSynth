a301b0c5336d9d911d7e58831f38aa88cdbc0bd4
alvaradocx <42775228+alvaradocx@users.noreply.github.com>
2022-06-21 13:15:37 -0400

    Update os_multi.py

--- os_multi.py
+++ os_multi.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from os_apps import gene_search, home, data, analysis, top_genes, snp_search, drug_search, data_sources, network, gene_snp # import your app modules here
+from os_apps import home, data, analysis, top_genes, drug_search, data_sources, network, gene_snp # import your app modules here
 
 
 app = MultiApp()
@@ -16,8 +16,6 @@ app.add_app("Data", data.app)
 app.add_app("Analysis", analysis.app)
 app.add_app("Top Genes & SNPs", top_genes.app)
 app.add_app("Gene and SNP search", gene_snp.app)
-#app.add_app("Gene Search", gene_search.app)
-#app.add_app("SNP Search", snp_search.app)
 app.add_app("Drug Target Search", drug_search.app)
 app.add_app("Gene Network", network.app)
 app.add_app("Data Sources and Information", data_sources.app)
